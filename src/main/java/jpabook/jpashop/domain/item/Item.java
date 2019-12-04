a56d1ea5005df797d5046460c946bc9613417853
Jo-GyuHyeon <kuhyun1993@gmail.com>
2019-12-04 22:34:50 +0900

    added: 상품 기능 개발  - 상품 기능 개발 & 테스트 케이스

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.CategoryItem;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,17 @@ public abstract class Item {
 
     @OneToMany(mappedBy = "item")
     private List<CategoryItem> categoryItems = new ArrayList<CategoryItem>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
+
---
+++ src/test/java/jpabook/jpashop/ItemRepositoryTest.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//Transactional 을 여기서 작성하여도 가능하다.
+public class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    //엔티티 매니저를 통한 데이터 변경은 트렌젝션안에서 이루어져야 한다.
+    @Transactional
+    //데이터를 DB에 저장하고 싶다면 rollback을 false 하여야 한다.
+    @Rollback(false)
+    public void testMember() {
+        //given
+        Item item = new Book();
+        item.setName("itemBook");
+
+        //when
+        itemRepository.save(item);
+        Long savedId = 1L;
+        Item findItem = itemRepository.findOne(savedId);
+
+        //then
+        Assertions.assertThat(findItem.getId()).isEqualTo(item.getId());
+        Assertions.assertThat(findItem.getName()).isEqualTo(item.getName());
+        Assertions.assertThat(findItem).isEqualTo(item); //JPA 엔티티 동일성 보장
+    }
+
+}
