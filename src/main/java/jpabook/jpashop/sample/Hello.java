e344c96c51c10c99e83da5286532011c7fb92334
Jo-GyuHyeon <kuhyun1993@gmail.com>
2019-12-06 17:48:25 +0900

    added: 웹 계층 개발 - home

--- src/main/java/jpabook/jpashop/sample/Hello.java
+++
@@ -1,9 +0,0 @@
-package jpabook.jpashop.sample;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class Hello {
-    private  String date;
-}
--- src/main/java/jpabook/jpashop/web/HelloController.java
+++ src/main/java/jpabook/jpashop/web/HelloController.java
@@ -1,15 +1,18 @@
-package jpabook.jpashop.sample;
+package jpabook.jpashop.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
+        log.info("hello controller");
         return "hello";
     }
 }
