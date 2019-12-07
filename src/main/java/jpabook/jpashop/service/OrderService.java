be6f0628761c55a823ff443584e3dfabae758b3b
Jo-GyuHyeon <kuhyun1993@gmail.com>
2019-12-07 12:15:29 +0900

    added: 주문 주문 기능 추가

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,15 +1,14 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.domain.Delivery;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -48,10 +47,13 @@ public class OrderService {
         //주문 취소
         order.cancel();
     }
-    /** 주문 검색 */
-/*
- public List<Order> findOrders(OrderSearch orderSearch) {
- return orderRepository.findAll(orderSearch);
- }
-*/
+
+    /**
+     * 주문 검색
+     */
+
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
+
 }
